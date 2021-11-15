ae25651816e80bafea4476047fc49d54468cc98e
szy0syz <szy0syz@gmail.com>
2021-11-15 17:47:18 +0800

    init

--- android/app/src/main/kotlin/com/example/flutter_card_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_card_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const CARD_WIDTH = 226 / 2;
-const CARD_HEIGHT = 314 / 2;
-
-const GS_LAST_SUIT = 'GS_LAST_SUIT';
-const GS_LAST_VALUE = 'GS_LAST_VALUE';
